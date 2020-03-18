5deceefd3c3625a59f39945a2ed0d41b63b9fae4
Alexander Dess <adess@vmware.com>
2020-03-18 10:00:50 +0100

    example added

--- on-prem-to-cloud-docker.ps1
+++ on-prem-to-cloud-docker.ps1
@@ -6,10 +6,13 @@
 .NOTES  Docker Command: docker run --rm -it --entrypoint='/usr/bin/pwsh' \
     -v /Users/adess/scripts:/tmp/scripts vmware/powerclicore /tmp/scripts/on-prem-to-cloud-docker.ps1 -onpremHCX 172.30.0.171 -onpremHCXUser administrator@vsphere.local -onpremHCXPassword VMware1! -HCXNetworkCloud L2E_vds-emea-stretched-0-1b039c1f -HCXNetworkOnPrem vds-emea-stretched -vmName MigrateVM-11
 
-docker run --rm -it adess/hcx-migration \
-    -e onpremHCXPassword=VMware1!
+
+# Pass variables to the runtime:
+docker run -e "onpremHCXPassword=VMware1" -it adess/hcx-migration
 
 #>
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
+
 $onpremHCXPassword = (Get-ChildItem ENV:onpremHCXPassword).Value
 
 <#
@@ -34,7 +37,6 @@ param(
 )
 #>
 
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
 
 
 
